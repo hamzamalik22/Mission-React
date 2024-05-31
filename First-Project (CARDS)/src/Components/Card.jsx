bb9192aeb3e9809b7343d49eaaea45a9fe1458c7
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-05-31 23:23:13 +0500

    Conditional Rendering & Event Handling Learnt

--- First-Project (CARDS)/src/Components/Card.jsx
+++ First-Project (CARDS)/src/Components/Card.jsx
@@ -7,27 +7,36 @@ function Card() {
         "https://images.unsplash.com/photo-1716847214569-631cb7f1751c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Amazon Basics",
       description:
-        "ODE\resourcesappout\vsworkbenchapi\nodeextensionHostProcess.js:153:99850)",
+        "ODE resourcesappout vswork benchapi node extension HostProcess .js:153:9 9850)",
+      inStock: true,
     },
     {
       image:
         "https://images.unsplash.com/photo-1716927856017-ec6dd4e4021e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Laura Cleffmann",
       description: "Free to use under the Unsplash License, FUJIFILM, X-T30 II",
+      inStock: false,
     },
     {
       image:
         "https://images.unsplash.com/photo-1716983541742-1a2bb3d2b5b1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Alexander Grigoryev",
       description:
-        "Women Images & Pictures FemaleHumanPeople Images & PicturesAdult",
+        "Women Images & Pictures Female Human People Images & Pictures Adult",
+      inStock: true,
     },
   ];
 
+  const AddToCart = ()=>{
+    alert('Item Added to Cart');
+  }
+
+
+
   return (
-    <div className="bg-zinc-600 py-2 px-2 w-full h-full flex  gap-10 justify-center items-center">
+    <div className="bg-zinc-100 py-2 px-2 w-full h-screen flex gap-10 justify-center items-center">
       {info.map((elem, index) => (
-        <div className="card w-56 rounded-lg overflow-hidden bg-slate-200">
+        <div className="card w-56 h-72 rounded-lg overflow-hidden bg-slate-200">
           <div className="image w-full h-32">
             <img
               className="w-full h-full object-cover"
@@ -39,7 +48,12 @@ function Card() {
             <h1 className="font-semibold ">{elem.title}</h1>
           </div>
           <div className="para py-2 px-2">
-            <p className="text-xs">{elem.description}</p>
+            <p className="text-xs h-12">{elem.description}</p>
+          </div>
+          <div className="py-2 px-2">
+            <button onClick={elem.inStock ? AddToCart : ''} className={`py-2 px-3 ${elem.inStock ? 'bg-green-400' : 'bg-red-400'} text-xs font-semibold rounded`}>
+              {elem.inStock ? 'In Stock' : 'Out of Stock'}
+            </button>
           </div>
         </div>
       ))}
