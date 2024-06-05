205ad8878397199f3d8bf189a700d3b0f977e52d
hamzamalik22 <hamzamalikaj22@gmail.com>
2024-06-05 13:09:38 +0500

    Context Api learning Project init

---
+++ Context-Api-Project/src/utils/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+function Router() {
+  return (
+    <>
+        <Routes>
+
+        <Route path='' element />
+        <Route path='' element />
+        <Route path='' element />
+
+
+
+        </Routes>
+
+
+    </>
+  )
+}
+
+export default Router
